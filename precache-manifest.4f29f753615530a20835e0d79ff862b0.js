1e80e4934d34d721893ee6f2bc8be5081784d391
temmietope <temmieayodele@gmail.com>
2021-03-11 11:03:14 +0100

    Updates

---
+++ precache-manifest.4f29f753615530a20835e0d79ff862b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6722c4d119b8440d5b1845b66d596389",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a72e62126151f3ebaa23",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "e9859e5633dea716c2e7",
+    "url": "./static/js/2.377d22c6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.377d22c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72e62126151f3ebaa23",
+    "url": "./static/js/main.18c3f7fd.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.18c3f7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef19bb0ec1fb97aa1a5",
+    "url": "./static/js/runtime-main.0f252097.js"
+  }
+]);
